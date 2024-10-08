de8721f1839884358e0f9cbb119e1efbb745adce
davinawuy <alano.davin@office.ui.ac.id>
2024-10-08 20:15:25 +0700

    Moving class to forms

--- main/forms.py
+++ main/forms.py
@@ -2,6 +2,14 @@ from django.forms import ModelForm
 from main.models import Product
 from django.utils.html import strip_tags
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+
 
 #Create a form to add a product and clean the data
 class ProductForm(ModelForm):
@@ -18,3 +26,23 @@ class ProductForm(ModelForm):
         category = self.cleaned_data['category']
         return strip_tags(category)
     
+class CustomUserCreationForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        # Validate password strength
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            self.add_error('password', e)
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+
+        return cleaned_data
--- main/views.py
+++ main/views.py
@@ -16,6 +16,13 @@ from django.utils.html import strip_tags
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django import forms
+from main.forms import CustomUserCreationForm
+
+
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -84,31 +91,7 @@ def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.models import User
-from django import forms
 
-class CustomUserCreationForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
-
-        # Validate password strength
-        try:
-            validate_password(password)
-        except ValidationError as e:
-            self.add_error('password', e)
-
-        if password != confirm_password:
-            raise forms.ValidationError("Passwords do not match")
-
-        return cleaned_data
 
 
 def register_user(request):
